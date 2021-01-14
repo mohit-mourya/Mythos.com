42cd6813b98393fe49b2c1c6a92a605aa42e696e
Narsingh Jaiswal <nrsnghjaiswal@gmail.com>
2021-01-14 17:53:34 +0530

    correct image urls

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -13,38 +13,38 @@ export class SliderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  images = ['../../assets/ash-edmonds-OzMHgwxTolo-unsplash.jpg',
-  '../../assets/bruno-oliveira-_zqm6tWODWA-unsplash.jpg',
-  '../../assets/nitin-mendekar-WOIe_leUDLc-unsplash.jpg',
-  '../../assets/nicholas-safran-uEZMQ4ipjzY-unsplash.jpg',
-  '../../assets/jacek-dylag-JwWKV2gCPkE-unsplash.jpg',
-  '../../assets/skrewball-whiskey-kVEGUjQYGgM-unsplash.jpg',
-  '../../assets/michael-fousert-K85v7YM_7vk-unsplash.jpg',
-  '../../assets/jack-ward-yU9TNvk_jI8-unsplash.jpg',
-  '../../assets/wine/wine1.jpg',
-  '../../assets/wine/wine14.jpg',
-  '../../assets/wine/wine4.jpg',
-  '../../assets/wine/wine6.jpg',
-  '../../assets/wine/wine8.jpg',
-  '../../assets/wine/wine5.jpg',
-  '../../assets/wine/wine9.jpg',
-  '../../assets/wine/wine11.jpg',
-  '../../assets/wine/wine14.jpg',
-  '../../assets/wine/wine12.jpg',
-  '../../assets/wine/wine16.jpg',
-  '../../assets/wine/wine13.jpg',
-  '../../assets/wishky/wiskey.jpg',
-  '../../assets/wishky/wiskey1.jpg',
-  '../../assets/wishky/wiskey2.jpg',
-  '../../assets/wishky/wiskey3.jpg',
-  '../../assets/wishky/wiskey4.jpg',
-  '../../assets/wishky/wiskey5.jpg',
-  '../../assets/wishky/wiskey6.jpg',
-  '../../assets/wishky/wiskey7.jpg',
-  '../../assets/wishky/wiskey8.jpg',
-  '../../assets/wishky/wiskey9.jpg',
-  '../../assets/wishky/wiskey10.jpg',
-  '../../assets/wishky/wiskey11.jpg'
+  images = ['https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/ash-edmonds-OzMHgwxTolo-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/bruno-oliveira-_zqm6tWODWA-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/nitin-mendekar-WOIe_leUDLc-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/nicholas-safran-uEZMQ4ipjzY-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/jacek-dylag-JwWKV2gCPkE-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/skrewball-whiskey-kVEGUjQYGgM-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/michael-fousert-K85v7YM_7vk-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/jack-ward-yU9TNvk_jI8-unsplash.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine1.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine14.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine4.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine6.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine8.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine5.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine9.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine11.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine14.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine12.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine16.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wine/wine13.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey1.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey2.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey3.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey4.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey5.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey6.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey7.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey8.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey9.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey10.jpg?raw=true',
+  'https://github.com/mohit-mourya/Mythos.com/blob/master/src/assets/wishky/wiskey11.jpg?raw=true'
  ]
 
   constructor(config: NgbCarouselConfig) {
@@ -56,5 +56,5 @@ export class SliderComponent implements OnInit {
   }
 
 
-  
+
 }
